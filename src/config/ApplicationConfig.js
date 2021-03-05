344013524d59255fb7f42ec746245a0e5ccf8b1a
Administrator <admin@example.com>
2021-03-05 14:04:25 +0200

    173

--- src/config/ApplicationConfig.js
+++ src/config/ApplicationConfig.js
@@ -1,4 +1,4 @@
-export const ROOT_SERVER = "${DESTINATION_URL}";
+export const ROOT_SERVER = "http://192.168.242.173";
 export const ROOT_URI = "/dev";
 
 export const BASE_URL = ROOT_SERVER + ROOT_URI;
